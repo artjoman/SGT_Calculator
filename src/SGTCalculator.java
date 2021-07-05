f881a885dcd2951950810dfd097c669ffc6feecf
shegoestech202122 <artjoms.v.petrovs@gmail.com>
2021-07-05 17:25:21 +0300

    Initial commit

---
+++ src/SGTCalculator.java
@@ -0,0 +1,25 @@
+public class SGTCalculator {
+
+    public static void main(String[] args) {
+
+        // Define variables
+        int varA, varB, varC;
+        double varX, VarY, varZ;
+        double result;
+
+        // Variant 1. Artjoms P.
+        // a^3 + 5*b + sqrt(x)
+        // a = 7, b = 95, x = 179.62
+        varA = 7;
+        varB = 95;
+        varX = 179.62;
+
+        result = Math.pow(varA,3) + 5*varB + Math.sqrt(varX);
+        System.out.println("Variant 1. Artjoms P.");
+        System.out.println(result);
+
+        // Variants 2+ should go below this line
+
+
+    }
+}
