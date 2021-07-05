7d021361860747d611f7257ce1c7dbce687bef4a
laura_martinsone <simsone.laura@gmail.com>
2021-07-05 22:03:52 +0300

    LauraM calculator changes

--- src/SGTCalculator.java
+++ src/SGTCalculator.java
@@ -4,7 +4,8 @@ public class SGTCalculator {
 
         // Define variables
         int varA, varB, varC;
-        double varX, VarY, varZ;
+        double varX, varY, varZ;
+
         double result;
 
         // Variant 1. Artjoms P.
@@ -19,6 +20,22 @@ public class SGTCalculator {
         System.out.println(result);
 
         // Variants 2+ should go below this line
+        // 5*a - 3*b + c^7 - x
+        // a=6, b=4, c=2, x=1.15
+
+
+        varA = 6;
+        varB = 4;
+        varC = 2;
+        varX = 1.15;
+
+        result = 5*varA - 3*varB +  Math.pow(varC,7) - varX;
+        System.out.println("Variant 12. LauraM");
+        System.out.println(result);
+
+
+
+
 
     }
 }
