f435f6f18c03b330f0bd1e483766fa10ae322dee
lauravendele <laura.vendele@gmail.com>
2021-07-06 00:14:57 +0300

    Please, DO NOT ACCEPT in main code!!! This branch is just my own learning and training activity.

--- src/SGTCalculator.java
+++ src/SGTCalculator.java
@@ -17,6 +17,19 @@ public class SGTCalculator {
         result = Math.pow(varA,3) + 5*varB + Math.sqrt(varX);
         System.out.println("Variant 1. Artjoms P.");
         System.out.println(result);
+        System.out.println(" ");
+
+        // Variant 2. Nikola.
+        // 2*a + 3*b - x^2
+        // a=7, b=2, x = 1.55
+        varA = 7;
+        varB = 2;
+        varX = 1.55;
+
+        result = 2*(varA) + 3*(varB) - Math.pow(varX, 2);
+        System.out.println("Variant 2. Nikola.");
+        System.out.println(result);
+        System.out.println(" ");
 
         // Variant 3. LauraV.
         // 7*a + 12.5*b + x +5*y
@@ -29,8 +42,244 @@ public class SGTCalculator {
         result = 7*(varA) + 12.5*(varB) + varX + 5*(varY);
         System.out.println("Variant 3. LauraV.");
         System.out.println(result);
+        System.out.println(" ");
 
+        // Variant 4. Modrite.
+        // 5*a - 3*b + c^7 - x
+        // a=6, b=4, c=2, x=1.15
+        varA = 6;
+        varB = 4;
+        varC = 2;
+        varX = 1.15;
 
+        result = 5*(varA) - 3*(varB) + Math.pow(varC, 7) - varX;
+        System.out.println("Variant 4. Modrite.");
+        System.out.println(result);
+        System.out.println(" ");
+
+        // Variant 5. Tatjana.
+        // 9*a + sqrt(2*b) -c
+        // a=7, b=26, c=16
+        varA = 7;
+        varB = 26;
+        varC = 16;
+
+        result = 9*(varA) + Math.sqrt(2*varB) - varC;
+        System.out.println("Variant 5. Tatjana.");
+        System.out.println(result);
+        System.out.println(" ");
+
+        // Variant 6. Kristine.
+        // 2*a + 3*b - x^2
+        // a=7, b=2, x = 1.55
+        varA = 7;
+        varB = 2;
+        varX = 1.55;
+
+        result = 2*(varA) + 3*varB - Math.pow(varX, 2);
+        System.out.println("Variant 6. Kristine.");
+        System.out.println(result);
+        System.out.println(" ");
+
+        // Variant 7. Jelizaveta.
+        // 7*a + 12.5*b + x +5*y
+        // a=5, b=1, x=7.01, y=3
+        varA = 5;
+        varB = 1;
+        varX = 7.01;
+        varY = 3;
+
+        result = 7*varA + 12.5*varB + varX + 5*varY;
+        System.out.println("Variant 7. Jelizaveta.");
+        System.out.println(result);
+        System.out.println(" ");
+
+        // Variant 8. Zanda.
+        // 5*a - 3*b + c^7 - x
+        // a=6, b=4, c=2, x=1.15
+        varA = 6;
+        varB = 4;
+        varC = 2;
+        varX = 1.15;
+
+        result = 5*varA - 3*varB + Math.pow(varC, 7) - varX;
+        System.out.println("Variant 8. Zanda.");
+        System.out.println(result);
+        System.out.println(" ");
+
+        // Variant 9. Marika.
+        // 9*a + sqrt(2*b) -c
+        // a=7, b=26, c=16
+        varA = 7;
+        varB = 26;
+        varC = 16;
+
+        result = 9*varA + Math.sqrt(2*varB) - varC;
+        System.out.println("Variant 9. Marika.");
+        System.out.println(result);
+        System.out.println(" ");
+
+        // Variant 10. Livija.
+        // 2*a + 3*b - x^2
+        // a=7, b=2, x = 1.55
+        varA = 7;
+        varB = 2;
+        varX = 1.55;
+
+        result = 2*varA + 3*varB - Math.pow(varX,2);
+        System.out.println("Variant 10. Livija.");
+        System.out.println(result);
+        System.out.println(" ");
+
+        // Variant 11. Baiba.
+        // 7*a + 12.5*b + x +5*y
+        // a=5, b=1, x=7.01, y=3
+        varA = 5;
+        varB = 1;
+        varX = 7.01;
+        varY = 3;
+
+        result = 7*varA + 12.5*varB + varX + 5*varY;
+        System.out.println("Variant 11. Baiba.");
+        System.out.println(result);
+        System.out.println(" ");
+
+        // Variant 12. LauraM.
+        // 5*a - 3*b + c^7 - x
+        // a=6, b=4, c=2, x=1.15
+        varA = 6;
+        varB = 4;
+        varC = 2;
+        varX = 1.15;
+
+        result = 5*varA - 3*varB + Math.pow(varC,7) - varX;
+        System.out.println("Variant 12. LauraM.");
+        System.out.println(result);
+        System.out.println(" ");
+
+        // Variant 13. LauraL.
+        // 9*a + sqrt(2*b) -c
+        // a=7, b=26, c=16
+        varA = 7;
+        varB = 26;
+        varC = 16;
+
+        result = 9*varA + Math.sqrt(2*varB) - varC;
+        System.out.println("Variant 13. LauraL.");
+        System.out.println(result);
+        System.out.println(" ");
+
+        // Variant 14. Liene.
+        // 2*a + 3*b - x^2
+        // a=7, b=2, x = 1.55
+        varA = 7;
+        varB = 2;
+        varX = 1.55;
+
+        result = 2*varA + 3*varB - Math.pow(varX,2);
+                System.out.println("Variant 14. Liene.");
+        System.out.println(result);
+        System.out.println(" ");
+
+        // Variant 15. Sabine.
+        // 7*a + 12.5*b + x +5*y
+        // a=5, b=1, x=7.01, y=3
+        varA = 5;
+        varB = 1;
+        varX = 7.01;
+        varY = 3;
+
+        result = 7*varA + 12.5*varB + varX + 5*varY;
+                System.out.println("Variant 15. Sabine.");
+        System.out.println(result);
+        System.out.println(" ");
+
+        // Variant 16. Elina.
+        // 5*a - 3*b + c^7 - x
+        // a=6, b=4, c=2, x=1.15
+        varA = 6;
+        varB = 4;
+        varC = 2;
+        varX = 1.15;
+
+        result = 5*varA -3*varB + Math.pow(varC, 7) - varX;
+                System.out.println("Variant 16. Elina.");
+        System.out.println(result);
+        System.out.println(" ");
+
+        // Variant 17. Dace.
+        // 9*a + sqrt(2*b) -c
+        // a=7, b=26, c=16
+        varA = 7;
+        varB = 26;
+        varC = 16;
+
+        result = 9*varA + Math.sqrt(2*varB) - varC;
+                System.out.println("Variant 17. Dace.");
+        System.out.println(result);
+        System.out.println(" ");
+
+        // Variant 18. Anastasija.
+        // 2*a + 3*b - x^2
+        // a=7, b=2, x = 1.55
+        varA = 7;
+        varB = 2;
+        varX = 1.55;
+
+        result = 2*varA + 3*varB - Math.pow(varX, 2);
+                System.out.println("Variant 18. Anastasija.");
+        System.out.println(result);
+        System.out.println(" ");
+
+        // Variant 19. Marta.
+        // 7*a + 12.5*b + x +5*y
+        // a=5, b=1, x=7.01, y=3
+        varA = 5;
+        varB = 1;
+        varX = 7.01;
+        varY = 3;
+
+        result = 7*varA + 12.5*varB + varX + 5*varY;
+                System.out.println("Variant 19. Marta.");
+        System.out.println(result);
+        System.out.println(" ");
+
+        // Variant 20. Oksana.
+        // 5*a - 3*b + c^7 - x
+        // a=6, b=4, c=2, x=1.15
+        varA = 6;
+        varB = 4;
+        varC = 2;
+        varX = 1.15;
+
+        result =5*varA - 3*varB + Math.pow(varC, 7) - varX;
+                System.out.println("Variant 20. Oksana.");
+        System.out.println(result);
+        System.out.println(" ");
+
+        // Variant 21. Lasma.
+        // 9*a + sqrt(2*b) -c
+        // a=7, b=26, c=16
+        varA = 7;
+        varB = 26;
+        varC = 16;
+
+        result = 9*varA + Math.sqrt(2*varB) - varC;
+                System.out.println("Variant 21. Lasma.");
+        System.out.println(result);
+        System.out.println(" ");
+
+        // Variant 22. Agnija.
+        // 2*a + 3*b - x^2
+        // a=7, b=2, x = 1.55
+        varA = 7;
+        varB = 2;
+        varX = 1.55;
+
+        result = 2*varA + 3*varB - Math.pow(varX, 2);
+                System.out.println("Variant 22. Agnija.");
+        System.out.println(result);
+        System.out.println(" ");
 
     }
 }
